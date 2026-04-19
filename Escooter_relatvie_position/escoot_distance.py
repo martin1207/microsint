603f02192d7cdb4136dc754ce013e2582c1a1ff7
martin1207 <madejaeghere@gmail.com>
2026-04-19 17:01:59 +0200

    Refactor code structure for improved readability and maintainability

---
+++ Escooter_relatvie_position/escoot_distance.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from joblib import load, dump
+import joblib
+
+
+thredshold_dist=1000
+
+import joblib
+
+def dist_est(c_x, c_y):
+    distance_features, distance_model = joblib.load('/Users/martin.dejaeghere/PhD/Position_Estimation/escooter/distance_predictor_small.joblib')
+    image_data =pd.DataFrame([[c_x, c_y]], columns=["x_center", "y2"])
+    cur_dist = distance_model.predict(distance_features.transform(image_data))[0][0]
+    if cur_dist >thredshold_dist-10:
+        distance_features, distance_model = joblib.load('/Users/martin.dejaeghere/PhD/Position_Estimation/escooter/distance_predictor_big.joblib')
+        image_data =pd.DataFrame([[c_x, c_y]], columns=["x_center", "y2"])
+        cur_dist = distance_model.predict(distance_features.transform(image_data))[0][0]
+ 
+    return cur_dist if 0 <= cur_dist <= 2400 else float('inf')
+def ang_est(c_x):
+    angle_raw = (c_x / 5376) * 360
+    relative_angle = (angle_raw) % 360
+
+    if relative_angle > 180:
+        relative_angle -= 360
+
+    return relative_angle
+
+
+def get_ang_dist_escoot(c_x, c_y):
+    width = 5376
+    segment = width / 4  # 1344
+
+    if 0 < c_x < segment:
+        cur_dist = dist_est(c_x, c_y)
+        cur_ang = ang_est(c_x)
+
+    elif segment <= c_x < 2 * segment:
+        c_x -= segment
+        cur_dist = dist_est(c_x, c_y)
+        cur_ang = ang_est(c_x)
+        cur_ang += 90
+
+
+
+    elif -segment < c_x < 0:
+        c_x = -c_x
+        cur_dist = dist_est(c_x, c_y)
+        cur_ang = ang_est(c_x)
+        cur_ang = -cur_ang
+
+    elif -2 * segment < c_x <= -segment:
+        c_x = -c_x - segment
+        cur_dist = dist_est(c_x, c_y)
+        cur_ang = ang_est(c_x)
+        cur_ang = -cur_ang - 90
+
+ 
+
+    else:
+        cur_dist = float('inf')
+        cur_ang = float('inf')
+
+    return cur_ang, cur_dist
+
+
+
